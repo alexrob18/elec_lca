5406fdc113d62c5b573eec3cf188d41d3d053266
AmyotJ <Janie.Amyot@ec.gc.ca>
2024-05-22 12:17:01 -0400

    Add functions to read the input template excel file

--- elec_lca/__init__.py
+++ elec_lca/__init__.py
@@ -3,8 +3,9 @@
 __all__ = (
     "__version__",
     # Add functions and variables you want exposed in `elec_lca.` namespace here
+    "read_input_excel_file",
 )
 
 __version__ = "0.0.1"
 
-from .read_inputs import *
+from .reading import read_input_excel_file
