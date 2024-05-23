b4669873900346d29b3c15811003436a5bde8553
matthieu-str <matthieu.souttre@gmail.com>
2024-05-23 09:55:41 -0400

    corrected function names

--- elec_lca/__init__.py
+++ elec_lca/__init__.py
@@ -6,7 +6,6 @@ __all__ = (
     "read_user_input_template_excel_file",
     "new_electricity_market",
     "create_user_input_file",
-    "technosphere_matrix",
     "get_coeff"
 )
 
@@ -15,4 +14,4 @@ __version__ = "0.0.1"
 from .reading import read_user_input_template_excel_file
 from .create_datasets import mapping, searching_dataset, new_electricity_market
 from .create_user_input_file import create_user_input_file
-from .lca_results import technosphere_matrix, get_coeff
+from .lca_results import *
