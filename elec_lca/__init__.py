b2419a0cdb3dd2c995b4b2ea900058a0d41f575a
matthieu-str <matthieu.souttre@gmail.com>
2024-05-22 21:42:35 -0400

    updated available functions for users

--- elec_lca/__init__.py
+++ elec_lca/__init__.py
@@ -4,10 +4,12 @@ __all__ = (
     "__version__",
     # Add functions and variables you want exposed in `elec_lca.` namespace here
     "read_user_input_template_excel_file",
-    "mapping",
-    "searching_dataset",
+    #"mapping",
+    #"searching_dataset",
     "new_electricity_market",
     "create_user_input_file",
+    "technosphere_matrix",
+    "get_coeff"
 )
 
 __version__ = "0.0.1"
@@ -15,3 +17,4 @@ __version__ = "0.0.1"
 from .reading import read_user_input_template_excel_file
 from .create_datasets import mapping, searching_dataset, new_electricity_market
 from .create_user_input_file import create_user_input_file
+from .lca_results import technosphere_matrix, get_coeff
