7f77436dd95bbcfcf0beaef676c065c60ed4ce22
matthieu-str <matthieu.souttre@gmail.com>
2024-05-23 09:39:30 -0400

    corrected A matrix functions

--- elec_lca/lca_results.py
+++ elec_lca/lca_results.py
@@ -1,30 +1,41 @@
 import bw2calc as bc
 import bw2data as bd
 
-def technosphere_matrix(database):
+def get_A_index(code_act, database):
     '''
-    Generates the A matrix of a given database
-    :param database: wurst database
-    :return: technosphere matrix object
+    Parameters
+    ----------
+    code_act: (str) code of the activity
+    database: wurst database
+
+    Returns
+    -------
+    Index of the activity in the A matrix
     '''
+    database_name = database[0]['database']
+    id = bd.get_id((database_name, code_act))
+
     act = (database[0]['database'], database[0]['code'])
     lca = bc.LCA(demand={act: 1}) # lca for random dataset
     lca.lci()
 
-    return lca.technosphere_matrix
-
+    return lca.dicts.activity[id]
 
 def get_coeff(code_act_1, code_act_2, database):
     '''
-    Get the coefficient
+    Get the coefficient in the A matrix
     :param act_1: (str) code of activity 1
     :param act_2: (str) code of activity 2
     :param database: wurst database of the two activities
-    :return:
+    :return: A matrix coefficient
     '''
     database_name = database[0]['database']
     id_1 = bd.get_id((database_name, code_act_1))
     id_2 = bd.get_id((database_name, code_act_2))
-    A = technosphere_matrix(database)
 
-    return A[id_1, id_2]
+    act = (database[0]['database'], database[0]['code'])
+    lca = bc.LCA(demand={act: 1})  # lca for random dataset
+    lca.lci()
+    A = lca.technosphere_matrix
+
+    return A[lca.dicts.activity[id_1], lca.dicts.activity[id_2]]
